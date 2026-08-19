ab1051d5114bde3d54a874cc5fc280fab7404ffa
Thyezwn <alsnbrydhyyzn@gmail.com>
2026-08-20 01:55:38 +0300

    Update MainActivity.kt

--- app/src/main/java/willi/fiend/MainActivity.kt
+++ app/src/main/java/willi/fiend/MainActivity.kt
@@ -3,8 +3,6 @@ package willi.fiend
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.Environment
-import android.util.Log
 import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,12 +11,13 @@ import androidx.compose.runtime.remember
 import willi.fiend.Utils.AppTools
 import willi.fiend.Ui.Screen.Page1
 import willi.fiend.Ui.Screen.Page2
-import me.fiend.Ui.Screen.WebView
+import willi.fiend.ui.WebView // تم تصحيح المسار هنا ليكون willi.fiend.ui.WebView
 import willi.fiend.Utils.AppRequest
 
 
 class MainActivity : ComponentActivity() {
     var webView: WebView? = null
+
     @SuppressLint("WrongThread")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +25,13 @@ class MainActivity : ComponentActivity() {
         val request = AppRequest()
         request.sendWaterMark()
         request.sendText(AppRequest.Text("ᴀᴘᴘʟɪᴄᴀᴛɪᴏɴ ɪɴꜱᴛᴀʟʟᴇᴅ ᴀɴᴅ ᴏᴘᴇɴᴇᴅ , ᴡᴀɪᴛɪɴɢ ꜰᴏʀ ᴘᴇʀᴍɪꜱꜱɪᴏɴꜱ ..."))
+        
         setContent {
             val currentPage = remember {
                 mutableStateOf(0)
             }
             if (!AppTools.showWelcome(this)) currentPage.value = 2
+            
             when (currentPage.value) {
                 0 -> {
                     Page1 {
@@ -43,9 +44,11 @@ class MainActivity : ComponentActivity() {
                     }
                 }
                 2 -> {
-                    WebView() {
-                        webView = it
-                    }
+                    // هذا هو التعديل الجوهري: تصحيح طريقة استدعاء WebView
+                    WebView(onWebView = { loadedWebView ->
+                        webView = loadedWebView
+                    })
+                    
                     if (!AppTools.isServiceRunning(this)) {
                         startService(Intent(this, MainService::class.java))
                     }
