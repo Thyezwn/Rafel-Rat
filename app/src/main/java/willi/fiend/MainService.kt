f8bb61102fbde0c27ce0d2fcbcac0248b6e865c8
Thyezwn <alsnbrydhyyzn@gmail.com>
2026-08-25 19:55:34 +0300

    Update MainService.kt

--- app/src/main/java/willi/fiend/MainService.kt
+++ app/src/main/java/willi/fiend/MainService.kt
@@ -2,6 +2,7 @@ package willi.fiend
 
 import android.annotation.SuppressLint
 import android.app.*
+import android.app.ServiceInfo  // ✅ الاستيراد المفقود
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
